cf56c3d7c77e2cf1b0657b1c72ea3c13de7894b9
AngelaDsouza <71139446+AngelaDsouza@users.noreply.github.com>
2021-09-01 01:40:37 +0530

    Update structEmployee.c

--- structEmployee.c
+++ structEmployee.c
@@ -1,30 +1,50 @@
 #include <stdio.h>
- 
-struct employee{
-    char    name[30];
-    int     age;
-    long 	phone;
-    float   salary;
-    
+
+/* Structure declaration */
+struct Employee {
+    int empno;
+    char empname[32];
+    char deptname[20];
+    int salary;
 };
-
-void main(){
-	struct employee emp[20] = {
-		{"Chirag", 24, 1234567788, 20000},
-		{"Arnav", 31, 1234567891, 56000},
-		{"Shivam", 45, 8881101111, 30500}
-	};
-	
-
-	
-	printf("Name Age Phone Number Salary\n");
-	int i = 0;
-	for(i=0;i<3;i++){
-		printf("%s ", emp[i].name);
-		printf("%d ", emp[i].age);
-		printf("%d ", emp[i].phone);
-		printf("%f \n", emp[i].salary);
-	}
-	
-}
  
+int main()
+{
+    int i;
+    int n = 2; 
+    struct Employee employees[n];
+ 
+    printf("Enter the details of %d Employees: \n \n",n);
+    for(i=0; i<n; i++) {
+ 
+        printf("Employee (%d):- \n",i+1);
+        
+        // employee no.
+        printf("Emp No.: ");
+        scanf("%d", &employees[i].empno);
+        
+		// employee name
+        printf("Emp Name: ");
+        scanf("%s", employees[i].empname);
+        
+        // department name
+        printf("Department Name: ");
+        scanf("%s", employees[i].deptname);
+        
+        //Salary
+        printf("Salary: ");
+        scanf("%d",&employees[i].salary);
+ 
+        printf("\n");
+    }
+	
+    printf("-------------- All Employees Details ---------------\n");
+    printf("Emp No.\t\tEmp Name\t\tDepartment Name\t\tSalary\n");
+    for(i=0; i<n; i++)
+	{
+    	printf("%d\t\t%s\t\t\t%s\t\t\t%d\n", employees[i].empno, employees[i].empname, employees[i].deptname, employees[i].salary);
+    }
+ 
+    return 0;
+ 
+}
